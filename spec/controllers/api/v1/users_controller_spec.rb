89c2c6f6c9da1562b6e19d304669f5b7c709e286
Andrei Laza <andrei@estudent.ro>
2015-08-19 16:33:33 +0300

    Adds show action to the user controller

---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Api::V1::UsersController do
+  before(:each) { request.headers['Accept'] = "application/vnd.estudent_api.v1" }
+
+  describe "GET #show" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      get :show, id: @user.id, format: :json
+    end
+
+    it "returns the information about the user in an json format" do
+      user_response = JSON.parse(response.body, symbolize_names: true)
+      expect(user_response[:email]).to eql @user.email
+    end
+
+    it { should respond_with 200 }
+  end
+
+end
