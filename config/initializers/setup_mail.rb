14ca81530b4996f903d786f79849eead799605d9
Andrei Laza <andrei@estudent.ro>
2015-09-29 11:43:04 +0300

    Sets sendrid variables to local_env

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'andreilaza',
-  :password => 'JkVSg3v)TX',
+  :user_name => ENV["SENDGRID_USERNAME"],
+  :password => ENV["SENDGRID_PASSWORD"],
   :domain => 'estudent.ro',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
-#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
