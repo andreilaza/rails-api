4f234eb02afee31e438d1d3697a289aedc8cf7e9
Andrei Laza <andrei@estudent.ro>
2015-10-21 16:59:04 +0300

    Updates access key

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -32,7 +32,7 @@ class Api::V1::SessionsController < ApplicationController
       # credentials = JSON.load(File.read('secrets.json'))
       credentials = {
         'AccessKeyId' => ENV["ADMIN_AWS_KEY_ID"],
-        'SecretAccessKey' => ENV["ADMIN_ADMIN_AWS_KEY_ID"],
+        'SecretAccessKey' => ENV["ADMIN_AWS_ACCESS_KEY"],
         'Bucket' => ENV["AWS_BUCKET"],
         'SeedBucket' => ENV["AWS_SEED_BUCKET"]
       } 
