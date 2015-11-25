70704642afe3cb1cd487269d856e8df011fdf6e3
Andrei Laza <andrei@estudent.ro>
2015-11-25 16:37:44 +0200

    Minor typo

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,5 +1,5 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :second_description, :published, :started, :progress, :completed, :duration, :institution, :cover_image, :author, :questions, :domain, :category, :teaser
+  attributes :id, :title, :description, :second_description, :published, :started, :progress, :completed, :duration, :institution, :cover_image, :author, :questions, :domain, :category, :teasers
 
   has_many :chapters
 
@@ -92,7 +92,7 @@ class CourseSerializer < ActiveModel::Serializer
     user
   end
 
-  def teaser
+  def teasers
     assets = Asset.where('entity_id' => object.id, 'entity_type' => 'course', 'definition' => 'teaser').all
     
     if assets
