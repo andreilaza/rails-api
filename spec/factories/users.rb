5e85d07dd3d011342eddffecccb4a4c41d7756e3
Andrei Laza <andrei@estudent.ro>
2015-12-28 10:48:48 +0200

    Minor UT settings

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,26 @@
 FactoryGirl.define do
-  factory :user do
-    email { 'asd@estudent.ro' }
-    password "12345678"
-    password_confirmation "12345678"
-    role 1
+  factory :user do    
+    password "password"
+    password_confirmation "password"
+
+    trait :admin do
+      email "stefan@estudent.ro"
+      role 1
+    end
+
+    trait :estudent do
+      email "andrei@estudent.ro"
+      role 2
+    end
+
+    trait :author do
+      email "coralia.sulea@e-uvt.ro"
+      role 3
+    end
+
+    trait :institution_admin do
+      email "coralia.sulea@e-uvt.ro"
+      role 4
+    end
   end
 end
