c34e8c049f6caba35b7a9a47e29048cedf27eb7b
Andrei Laza <andrei@estudent.ro>
2016-01-25 10:39:41 +0200

    Migrates data on authors and course status

--- db/migrate/20151208110747_create_author_courses.rb
+++ db/migrate/20151208110747_create_author_courses.rb
@@ -1,9 +1,21 @@
 class CreateAuthorCourses < ActiveRecord::Migration
-  def change
+  def up
     create_table :author_courses, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin' do |t|
       t.integer :user_id, default: 0
       t.integer :course_id, default: 0
       t.timestamps null: false
     end
+
+    course_institutions = CourseInstitution.all
+    course_institutions.each do |ci|
+      ac = AuthorCourse.new
+      ac.user_id = ci.user_id
+      ac.course_id = ci.course_id
+      ac.save
+    end
+  end
+
+  def down
+    drop_table :author_courses
   end
 end
--- db/migrate/20160106095434_change_published_to_status_on_course.rb
+++ db/migrate/20160106095434_change_published_to_status_on_course.rb
@@ -1,6 +1,30 @@
 class ChangePublishedToStatusOnCourse < ActiveRecord::Migration
-  def change
-    remove_column :courses, :published
+  def up
     add_column :courses, :status, :integer, default: 0
+    courses = Course.all
+    courses.each do |course|
+      if course.published
+        course.status = Course::STATUS[:published]
+      else
+        course.status = Course::STATUS[:unpublished]
+      end
+      course.save
+    end
+
+    remove_column :courses, :published
   end
+  
+  def down
+    add_column :courses, :published, :boolean, default: false
+    courses = Course.all
+    courses.each do |course|
+      if course.status == Course::STATUS[:published]
+        course.published = true
+      else
+        course.published = false
+      end
+      course.save
+    end
+    remove_column :courses, :status
+  end  
 end
